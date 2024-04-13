0d28f07c1a80900a4979bf81074007c332ff946d
Thulana2006 <thulanabaji@gmail.com>
2024-04-13 23:12:48 +0530

    Basic app routing configured
    authentication
    volunteer
    organization
    admin routings were added.

---
+++ app/vol/community/page.tsx
@@ -0,0 +1,11 @@
+import { Button, DarkThemeToggle } from "flowbite-react";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
+      <h1 className="text-2xl dark:text-white">Flowbite React + Next.js</h1>
+      <DarkThemeToggle />
+      
+    </main>
+  );
+}
