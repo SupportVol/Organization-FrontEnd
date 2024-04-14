e892dcdb99abb6c6e95c00e4df37b446e4953c1d
Ramesh Kaushika De Silva <149869212+Ramesh-Kaushika@users.noreply.github.com>
2024-04-14 11:52:34 +0530

    first commit Organization

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
       <h1 className="text-2xl dark:text-white">Flowbite React + Next.js</h1>
-      <DarkThemeToggle />
-      
+        <DarkThemeToggle />
+
     </main>
   );
 }
---
+++ components/organization/OrgCard.tsx
@@ -0,0 +1,10 @@
+import { Button, ButtonGroup } from "flowbite-react";
+import { HiAdjustments, HiCloudDownload, HiUserCircle } from "react-icons/hi";
+
+export default function OrgCard(props: any) {
+    return (
+        <div className="w-96 h-80 px-4 pt-4 pb-10 bg-white rounded-lg shadow border border-gray-200 flex-col justify-start items-start inline-flex">
+        <h1>Organization</h1>
+        </div>
+    );
+}
