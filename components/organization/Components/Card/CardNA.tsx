13b09536d632c8f9a68bb2f8214df1af486308bc
Ramesh Kaushika De Silva <149869212+Ramesh-Kaushika@users.noreply.github.com>
2024-04-15 00:35:55 +0530

    Card Components Done

--- components/organization/Components/Card/CardNA.tsx
+++ components/organization/Components/Card/CardNA.tsx
@@ -1,5 +1,5 @@
 import { Button, DarkThemeToggle } from "flowbite-react";
-
+import { Card } from "flowbite-react";
 import Image from "next/image";
 export default function CardNA() {
     return (
@@ -7,24 +7,17 @@ export default function CardNA() {
 
 
 
-
-            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-
-                    <img className="rounded-t-lg" src="/components/organization/assets/image-1.jpg" alt="" />
-
-                <div className="p-5">
-                    <a href="#">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                    </a>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Read more
-                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                        </svg>
-                    </a>
-                </div>
-            </div>
+            <Card
+                className="max-w-sm"
+                renderImage={() => <Image width={500} height={500} src='/components/organization/assets/image-1.jpg' alt="image 1" />}
+            >
+                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    Example News & Articles
+                </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                    Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                </p>
+            </Card>
 
 
 
---
+++ components/organization/Components/Card2/ComCard.tsx
@@ -0,0 +1,31 @@
+'use client';
+import {Button, Card} from "flowbite-react";
+import React, {useState} from "react";
+import Link from "next/link";
+
+import CardNA from "@/components/organization/Components/Card/CardNA";
+export default function ComCard(props: any) {
+
+    const [openModal, setOpenModal] = useState(false);
+
+    return (
+
+        <div >
+            <Card className="max-w-sm" imgSrc="/components/organization/assets/image-1.jpg" horizontal>
+                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    Example Community Name
+                </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                    Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                </p>
+                <div className="flex justify-end">
+                    <Button color="failure" onClick={() => setOpenModal(false)}>
+                        Join
+                    </Button>
+                </div>
+
+            </Card>
+        </div>
+
+    );
+}
