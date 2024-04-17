61c8293b7953e691b6d1d028517ce408a2959c2f
BKavsilu <bulan.kavsilu@gmail.com>
2024-04-17 10:06:02 +0530

    Updated

--- components/organization/Components/Card3/TraCard.tsx
+++ components/organization/Components/Card3/TraCard.tsx
@@ -78,7 +78,7 @@ export default function TraCard(props: any) {
                         <Datepicker />
                         <FloatingLabel variant="standard" label="Training Supervisor" />
                         <FloatingLabel variant="standard" label="Prerequisites" />
-                        <textarea id="description"  className=" resize-y h-40 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write product description here"></textarea>
+                        <textarea id="description"  className=" resize-y h-40 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write training description here"></textarea>
 
 
 
