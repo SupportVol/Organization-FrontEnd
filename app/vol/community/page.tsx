bebfacac7db4ab503d1b910614c084d9dca0bd32
Thulana2006 <thulanabaji@gmail.com>
2024-04-17 19:12:40 +0100

    Font configured

--- app/vol/community/page.tsx
+++ app/vol/community/page.tsx
@@ -2,23 +2,28 @@
 
 import Navigation from "@/components/volunteer/navigation";
 import CommunityCard from "@/ui-components/CommunityCard";
+import RoadmapButton from "@/ui-components/RoadmapButton";
 import { Button, DarkThemeToggle } from "flowbite-react";
-import { HiOutlineAcademicCap, HiOutlineHome, HiAcademicCap, HiHome} from "react-icons/hi"
+import {
+  HiOutlineAcademicCap,
+  HiOutlineHome,
+  HiAcademicCap,
+  HiHome,
+} from "react-icons/hi";
 
 export default function Home() {
-
   return (
-    <main className="flex flex-col font-inter min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
-      <DarkThemeToggle className="fixed top-10 right-10" />
+    <main className="font-inter flex min-h-screen flex-col items-center justify-center gap-2 dark:bg-gray-800">
+      <DarkThemeToggle className="fixed right-10 top-10" />
 
       <div className="flex flex-col items-center justify-center gap-2 dark:text-gray-100">
-        <h1 className="text-4xl font-bold text-center">Community</h1>
-        <p className="text-lg text-center">Welcome to the community page</p>
+        <h1 className="text-center text-4xl font-bold">Community</h1>
+        <p className="text-center text-lg">Welcome to the community page</p>
         <Button color="blue">Click me</Button>
 
         <CommunityCard />
+        <RoadmapButton />
       </div>
-
     </main>
   );
 }
